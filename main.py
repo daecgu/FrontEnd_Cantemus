b9343475dd29b7312afdf449be583199064ad93e
Daniel Echeverri <decheverri@helphone.com>
2023-11-15 12:17:46 +0100

    First Commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+import httpx
+
+"""uvicorn main:app --reload --port 3000"""
+app = FastAPI()
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.get("/listado-canciones")
+async def get_listado_canciones(request: Request):
+    return templates.TemplateResponse("listado_canciones.html", {"request": request})
+
+
+"""
+Estado de Sincronización: Si abres la ventana del reproductor más de una vez y cambias de diapositiva en una, 
+no se actualizará automáticamente en las otras ventanas a menos que implementes una forma de sincronización de estado,
+ como localStorage, sessionStorage, o WebSockets para mantener las ventanas sincronizadas.
+ 
+Implementar pre-cargar diapositivas o lazy loading para mejorar experiencia de usuario.
+Estilos y Pantalla Completa: Para los botones de control de pantalla completa y minimización, 
+necesitarás añadir lógica adicional en JavaScript para manejar estos casos. 
+Además, para el estilo y la funcionalidad de pantalla completa, 
+podrías necesitar algo de CSS adicional y usar la API de pantalla completa de HTML5.
+Seguridad: Asegúrate de validar y sanitizar los valores del ID de la canción para prevenir vulnerabilidades de seguridad.
+"""
+
+
+@app.get("/reproductor")
+async def get_reproductor(request: Request):
+    return templates.TemplateResponse("reproductor.html", {"request": request})
+
+
+
+"/canciones/{id_cancion}/diapositivas"
